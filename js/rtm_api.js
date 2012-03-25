252f709a21240d3ecb48fb7356db965d549d693f
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-03-25 13:13:03 -0700

    added first version of rtm_api can currently create a url with signature

---
+++ js/rtm_api.js
@@ -0,0 +1,36 @@
+function RTM (api_key, api_secret) {
+    this.api_key = api_key;
+    this.api_secret = api_secret;
+}
+
+RTM.prototype.authenticate = function() {
+    
+} 
+
+/**
+* @param params: the parameters other than the api_key and api_secret in form of a dictionary {param-name:param-value}
+*/
+RTM.prototype.formURL = function(serviceURL, params) {
+    hadAPIkey = true;
+    if ("api_key" in params) { 
+        params["api_key"] = this.api_key;
+        hadAPIkey = false;
+    }
+    param_names = keys(params).sort();
+    
+    sig = "" + api_secret;i
+    for (param_name in param_names) {
+        sig += param_name + param_names[param_name]
+    }
+    sig = hex_md5(sig);
+
+    url = serviceURL;
+    for (param_name in param_names) {
+        url += param_name "=" + params[param_name] + "&";
+    }
+    url += "api_sig=" + sig;
+
+    if (!hadAPIkey) delete params["api_key"];
+    
+    return url;
+}
