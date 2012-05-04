5f495a6c1bd1280ce7436dffaa64a5861bacc505
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-05-04 16:45:03 -0700

    close #22 - test in php if http:// is added and if not add it, might need to add a check later on for checking if https:// is present

--- php/authwindow.php
+++ php/authwindow.php
@@ -1,7 +1,16 @@
 <?php
-$auth_url = $_GET["url"];
+// from http://stackoverflow.com/questions/834303/php-startswith-and-endswith-functions
+function startsWith($haystack, $needle) {
+    $length = strlen($needle);
+    return (substr($haystack, 0, $length) === $needle);
+}
+
+$auth_url = urldecode($_GET["url"]);
 echo "<HTML><HEAD></HEAD><BODY>";
-echo '<iframe width="100%" height="90%" src="http://'.urldecode($auth_url).'"></iframe>';
+if (!startsWith($auth_url, "http://")) {
+  $auth_url = "http://".$auth_url;
+}
+echo '<iframe width="100%" height="90%" src="'.$auth_url.'"></iframe>';
 echo '<form>
 <input type="button" value="Close Window" onClick="window.close()">
 </form>';
