42928e277b4211cde1af71e546ef41b7684b3f92
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-05-08 13:35:01 -0700

    close #36 - remove frob from cookies when page is first opened

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 load = function() {
   // using the desktop api authentication
+  setCookie("frob","",-1);
   var rtm = new RTM();
   var frob = rtm.getFrob();
   window.document.write("frob before: " + frob + "<br></br>");
@@ -10,4 +11,6 @@ afterAuthentication = function(frob) {
   var rtm = new RTM();
   var frob = rtm.getFrob();
   window.document.write("frob after: " + frob + "<br></br>");
+  var auth_token = rtm.getAuthToken(frob);
+  window.document.write("auth token: " + auth_token + "<br></br>");
 }
