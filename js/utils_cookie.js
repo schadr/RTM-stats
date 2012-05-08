272fb2ed14e8b19fb28519a8a18327ea0924ca5b
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-05-08 09:55:33 -0700

    #26 - doing cookies stuff more correctly

--- js/utils_cookie.js
+++ js/utils_cookie.js
@@ -2,17 +2,11 @@ getCookieValue = function(c_name) {
   return getCookies[c_name];
 }
 
-setCookie = function(c_name_value, expires_in_days) {
+setCookie = function(c_name, c_value, expires_in_days) {
   var expiry_date = new Date();
-  expiry_date.setDate(expiry_date.getDate() + expires_in_days);
-  var cookie = "";
-  if (expires_in_days != null) cookie = cookie + "expires=" + expiry_date.toUTCString();
-  var cookie_names = keys(c_name_value);
-  for (var cookie_name in cookie_names) {
-    var cookie_value = c_name_value[cookie_name];
-    cookie = cookie + ";" + cookie_name + "=" + escape(cookie_value);
-  }
-  document.cookie = cookie;
+  expiry_date.setTime(exdate.getTime() + exdays*24*60*60*1000);
+  var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+  document.cookie=c_name + "=" + c_value;
 }
 
 getCookies = function() {
@@ -21,20 +15,28 @@ getCookies = function() {
   for (var i = 0; i < cookies.length; i++) {
     var cookie_name = cookies[i].substr(0, cookies[i].indexOf("="));
     cookie_name = cookie_name.replace(/^\s+|\s+$/g,"");
-    var cookie_value = cookies[i].substr(cookies[i].indecOf("=")+1);
-    r_cookies[cookie_name] = unescape(cookie_value);
+    var cookie_value = unescape(cookies[i].substr(cookies[i].indexOf("="));
+    r_cookies[cookie_name] = cookie_value;
   }
   return r_cookies;
 }
 
-isCookieExpired = function() {
-  var str_expiry_date = getCookies["expires"];
-  if (str_expiry_date == null || str_expiry_date == undefined) return false; 
-  var expiry_date = new Date(str_expiry_date);
-  var current_date = new Date();
-  return current_date > expiry_date;
+removeCookie = function(c_name) {
+  // to delete a cookie just set its expiration date before today
+  setCookie(c_name, "", -1);
 }
 
 clearCookies = function() {
-  document.cookies = null;
+  var cookies = getCookies();
+  for (var cookie_name in cookies) {
+    removeCookie(cookie_name);
+  }
+}
+
+updateCookie = function(c_name, c_value, expires_in_days) {
+  setCookie(c_name, c_value, expires_in_days)
+}
+
+hasCookie = function(c_name) {
+  return c_name in getCookies();
 }
