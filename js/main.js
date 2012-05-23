639dc4b4aa627ca301b52bce2b88a0d65975479a
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-05-23 13:07:14 -0700

    intermedate stage

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ load = function() {
   setCookie("frob","",-1);
   var rtm = new RTM();
   var frob = rtm.getFrob();
-  window.document.write("frob before: " + frob + "<br></br>");
+  console.log("frob before: " + frob + "<br></br>");
   rtm.authenticate(frob, afterAuthentication);
 }
 
 afterAuthentication = function(frob) {
   var rtm = new RTM();
   var frob = rtm.getFrob();
-  window.document.write("frob after: " + frob + "<br></br>");
+  console.log("frob after: " + frob + "<br></br>");
   var auth_token = rtm.getAuthToken(frob);
-  window.document.write("auth token: " + auth_token + "<br></br>");
+  console.log("auth token: " + auth_token + "<br></br>");
 }
--- js/rtm_api.js
+++ js/rtm_api.js
@@ -1,31 +1,31 @@
 function RTM() {
-    this.api_key = "42af200895280c26d736e11b9a02b7da";
+  this.api_key = "42af200895280c26d736e11b9a02b7da";
 }
 
 /**
 * @param params: the parameters other than the api_sig in form of a dictionary {param-name:param-value}
 */
 RTM.prototype.formURL = function(serviceURL, params) {
-    hadAPIkey = true;
-    if (!("api_key" in params)) { 
-        params["api_key"] = this.api_key;
-        hadAPIkey = false;
-    }
-    param_names = keys(params).sort();
-    sig = "";
-    for (i = 0; i < param_names.length; i = i + 1) {
-        sig = sig + param_names[i] + params[param_names[i]];
-    }
-    sig = this.sign(sig);
-    url = serviceURL + "?";
-    for (i = 0; i < param_names.length; i = i + 1) {
-        url = url + param_names[i] + "=" + params[param_names[i]] + "&";
-    }
-    url = url + "api_sig=" + sig;
+  hadAPIkey = true;
+  if (!("api_key" in params)) { 
+    params["api_key"] = this.api_key;
+    hadAPIkey = false;
+  }
+  param_names = keys(params).sort();
+  sig = "";
+  for (i = 0; i < param_names.length; i = i + 1) {
+    sig = sig + param_names[i] + params[param_names[i]];
+  }
+  sig = this.sign(sig);
+  url = serviceURL + "?";
+  for (i = 0; i < param_names.length; i = i + 1) {
+    url = url + param_names[i] + "=" + params[param_names[i]] + "&";
+  }
+  url = url + "api_sig=" + sig;
 
-    if (!hadAPIkey) delete params["api_key"];
+  if (!hadAPIkey) delete params["api_key"];
     
-    return url;
+  return url;
 }
 
 RTM.prototype.sign = function(payload) {
